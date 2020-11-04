ce1370e2b576d87016892708e77dd0177f2f4a77
Scott Widmer <info@scottwidmer.com>
2020-11-04 10:02:20 -0500

    removing console.log

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,7 @@ export default function Page(props) {
     },
     moduleStyleData
   } = props;
-  console.log('content: ' + content);
+  //console.log('content: ' + content);
   return (
     <MainLayout {...props}>
       <Head>
