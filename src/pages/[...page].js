0e6c97c91a858b5e319fc70d5918db8c7a527558
Matt Levine <matt.levine@blueriver.com>
2020-12-29 11:15:39 -0800

    Adding support for 404s

--- src/pages/[...page].js
+++ src/pages/[...page].js
@@ -3,6 +3,7 @@ import Head from 'next/head';
 import { getMuraProps, getRootPath, getMuraPaths, getSiteName } from '@mura/react/MuraConnector';
 import MainLayout from '@mura/react/UI/MainLayout';
 import DisplayRegion from '@mura/react/UI/DisplayRegion';
+import ErrorPage from 'next/error'
 
 export async function getStaticPaths() {
   const paths = await getMuraPaths();
@@ -36,64 +37,68 @@ export default function Page(props) {
     moduleStyleData
   } = props;
 
-  return (
-    <MainLayout {...props}>  
-      <Head>
-        {/* I wanted to add a "MuraMetaTags" component here but doesn't seem possible inside the <Head> component -- see metaTags branch */}
-        <title>{content.htmltitle} - {getSiteName()}</title>
-        <meta name="description" content={content.metadesc} />
+  if(content.isnew){
+    return <ErrorPage statusCode="404" />
+  } else {
+    return (
+      <MainLayout {...props}>  
+        <Head>
+          {/* I wanted to add a "MuraMetaTags" component here but doesn't seem possible inside the <Head> component -- see metaTags branch */}
+          <title>{content.htmltitle} - {getSiteName()}</title>
+          <meta name="description" content={content.metadesc} />
 
-        <meta property="og:site_name" content={getSiteName()} />
-        <meta property="og:title" content={content.htmltitle} />
-        <meta property="og:description" content={content.metadesc} />
-        {content.images && content.images.large &&
-          <meta property="og:image" content={content.images.large} />
-        }
-        <meta property="og:type" content="website" />
-        
-        {content.canonicalurl != '' &&
-          <link rel="canonical" href={content.canonicalurl} />
-        }
+          <meta property="og:site_name" content={getSiteName()} />
+          <meta property="og:title" content={content.htmltitle} />
+          <meta property="og:description" content={content.metadesc} />
+          {content.images && content.images.large &&
+            <meta property="og:image" content={content.images.large} />
+          }
+          <meta property="og:type" content="website" />
+          
+          {content.canonicalurl != '' &&
+            <link rel="canonical" href={content.canonicalurl} />
+          }
 
-        {content.canonicalurl == '' &&
-          <link rel="canonical" href={`${getRootPath()}/${content.filename}`} />
-        }
+          {content.canonicalurl == '' &&
+            <link rel="canonical" href={`${getRootPath()}/${content.filename}`} />
+          }
 
-        <link
-          href={`${getRootPath()}/core/modules/v1/core_assets/css/mura.10.min.css`}
-          rel="stylesheet"
-          key="min"
-        />
-        <link
-          href={`${getRootPath()}/core/modules/v1/core_assets/css/mura.10.skin.css`}
-          rel="stylesheet"
-          key="skin"
-        />
-        <div dangerouslySetInnerHTML={{__html:props.codeblocks.header}}/>
-      </Head>
-      <div dangerouslySetInnerHTML={{__html:props.codeblocks.bodystart}}/>
-      {content && displayregions && header && (
-        <DisplayRegion
-          region={header}
-          moduleStyleData={moduleStyleData}
-          content={content}
-        />
-      )}
-      {content && displayregions && primarycontent && (
-        <DisplayRegion
-          region={primarycontent}
-          moduleStyleData={moduleStyleData}
-          content={content}
-        />
-      )}
-       {content && displayregions && footer && (
-        <DisplayRegion
-          region={footer}
-          moduleStyleData={moduleStyleData}
-          content={content}
-        />
-      )}
-       <div dangerouslySetInnerHTML={{__html:props.codeblocks.footer}}/>
-    </MainLayout>
-  );
+          <link
+            href={`${getRootPath()}/core/modules/v1/core_assets/css/mura.10.min.css`}
+            rel="stylesheet"
+            key="min"
+          />
+          <link
+            href={`${getRootPath()}/core/modules/v1/core_assets/css/mura.10.skin.css`}
+            rel="stylesheet"
+            key="skin"
+          />
+          <div dangerouslySetInnerHTML={{__html:props.codeblocks.header}}/>
+        </Head>
+        <div dangerouslySetInnerHTML={{__html:props.codeblocks.bodystart}}/>
+        {content && displayregions && header && (
+          <DisplayRegion
+            region={header}
+            moduleStyleData={moduleStyleData}
+            content={content}
+          />
+        )}
+        {content && displayregions && primarycontent && (
+          <DisplayRegion
+            region={primarycontent}
+            moduleStyleData={moduleStyleData}
+            content={content}
+          />
+        )}
+        {content && displayregions && footer && (
+          <DisplayRegion
+            region={footer}
+            moduleStyleData={moduleStyleData}
+            content={content}
+          />
+        )}
+        <div dangerouslySetInnerHTML={{__html:props.codeblocks.footer}}/>
+      </MainLayout>
+    );
+  }
 }
