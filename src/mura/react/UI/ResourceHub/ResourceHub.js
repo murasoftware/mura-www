3d4c6d453bd86dff5392d6deb58415d17079f840
Scott Widmer <info@scottwidmer.com>
2020-12-30 12:32:40 -0500

    adding dropdowns for subtype, categoryid and personaid selection, updating filterProps

--- src/mura/react/UI/ResourceHub/ResourceHub.js
+++ src/mura/react/UI/ResourceHub/ResourceHub.js
@@ -4,6 +4,7 @@ import Collection from '@mura/react/UI/Collection';
 
 import ReactMarkdown from "react-markdown";
 import Card from 'react-bootstrap/Card';
+import Form from 'react-bootstrap/Form';
 import CollectionNav from '@mura/react/UI/CollectionNav/CollectionNav';
 import ItemDate from '@mura/react/UI/Utilities/ItemDate';
 import CollectionReadMoreBtn from '@mura/react/UI/Utilities/CollectionReadMoreBtn';
@@ -29,7 +30,8 @@ function ResourceHub(props) {
       return (
         <div>
           <h1>Dynamic {thisTitle}</h1>
-          {/* <Collection collection={collection} layout="List" /> */}
+          <RenderFilterForm />
+          {/* <Collection collection={collection} {...props} layout="List" /> */}
           <div className="row collectionLayoutCards row-cols-1 row-cols-sm-2 row-cols-md-2 row-cols-lg-3 row-cols-xl-3">
             <CurrentItems collection={collection} {...props} /> 
           </div>
@@ -118,8 +120,8 @@ export const getDynamicProps = async props => {
         props.content=props.content.getAll();
     }
 
-    const filterProps = await getFilterProps();//stopping dynamicProps from finishing?
-    console.log('filterprops: ' + JSON.stringify(filterProps, undefined, 2));
+    const filterProps = await getFilterProps('','','');
+    // console.log('filterprops: ' + JSON.stringify(filterProps, undefined, 2));
 
     const excludeIDList=props.content.contentid;
 
@@ -169,10 +171,15 @@ export const getDynamicProps = async props => {
     };
 }
 
-const getFilterProps = async props => {
-  const filterProps = await Mura.getEntity('resourcehub').invoke('processFilterArgs',{})
+const getFilterProps = async (subtype,categoryid,personaid) => {
+  const Subtype = subtype;
+  const Categoryid = categoryid;
+  const Personaid = personaid;
+
+  //console.log('getFilterProps Subtype: ' + Subtype);
+  const filterProps = await Mura.getEntity('resourcehub').invoke('processFilterArgs',{subtype:Subtype,categoryid:Categoryid,personaid:Personaid})
   .then(function(result) {
-    //console.log(result); // "initResolve"
+    console.log('filterprops: ' + JSON.stringify(result, undefined, 2));
     return result;
   });
   // console.log('filterprops: ' + filterProps);
@@ -180,22 +187,119 @@ const getFilterProps = async props => {
   // console.log('subtype: ' + filterProps.subtype);
   // console.log('categoryid: ' + filterProps.categoryid);
   // console.log('personaid: ' + filterProps.personaid);
+
+  // props.resourcefilters = {};
+  // props.resourcefilters.hasfilter = filterprops.hasfilter;
+  // props.resourcefilters.subtype = filterprops.subtype;
+  // props.resourcefilters.categoryid = filterprops.categoryid;
+  // props.resourcefilters.personaid = filterprops.personaid;
+
   return filterProps;
 }
 
-//for debugging only
-const replacerFunc = () => {
-    const visited = new WeakSet();
-    return (key, value) => {
-      if (typeof value === "object" && value !== null) {
-        if (visited.has(value)) {
-          return;
-        }
-        visited.add(value);
-      }
-      return value;
-    };
-  };
-//for debugging only
+const RenderFilterForm = () => {
+  const subtypesArray = [
+    {
+      key: 'whitepaper',
+      text: 'White Paper',
+      value: 'whitepaper',
+    },
+    {
+      key: 'webinar',
+      text: 'Webinar',
+      value: 'webinar',
+    }
+  ]
+
+  const categoriesArray = [
+    {
+      key: 'developers',
+      text: 'Developers',
+      value: '7EC9592C-719A-4745-B017ED8B6223178F',
+    },
+    {
+      key: 'itprofessionals',
+      text: 'IT Professionals',
+      value: 'C501C4E8-429E-408D-94721ADC3BA56E9F',
+    },
+    {
+      key: 'marketers',
+      text: 'Marketers',
+      value: '38212296-E460-4883-A1C83D49E3DAC901',
+    }
+  ]
+  const personasArray = [
+    {
+      key: 'persona1',
+      text: 'Persona 1',
+      value: 'persona1-id',
+    },
+    {
+      key: 'persona2',
+      text: 'Persona 2',
+      value: 'persona2-id',
+    }
+  ]
+  
+  // console.log('rendering dropdown...');
+  // console.log(subtypesArray);
+  const handleChange = (e) => {
+    //console.log('handling change...');
+    let subtype = '';
+    let categoryid = '';
+    let personaid = '';
+
+    //console.log('field name:' + e.target.name + ', field value:' + e.target.value);
+
+    switch(e.target.name) {
+      case 'subtype':
+        subtype = e.target.value;
+        break
+      case 'categoryid':
+        categoryid = e.target.value;
+        break
+      case 'personaid':
+        personaid = e.target.value;
+        break
+    }
+
+    getFilterProps(subtype,categoryid,personaid);
+  }
+
+  return (
+    <Form>
+      <Form.Group controlId="exampleForm.SelectCustom">
+        <Form.Label>Subtypes:</Form.Label>
+        <Form.Control as="select" name="subtype" custom onChange={ handleChange }>
+        <option value="" key="All Subtypes">All Subtypes</option>
+        {subtypesArray.map(option => (
+          <option value={option.value} key={option.key}>{option.text}</option>
+        ))}
+        </Form.Control>
+      </Form.Group>
+
+      <Form.Group>
+      <Form.Label>Categories:</Form.Label>
+        <Form.Control as="select" name="categoryid" custom onChange={ handleChange }>
+          <option value="" key="All Categories">All Categories</option>
+          {categoriesArray.map(option => (
+            <option value={option.value} key={option.key}>{option.text}</option>
+          ))}
+        </Form.Control>
+      </Form.Group>
+
+      <Form.Group>
+      <Form.Label>Personas:</Form.Label>
+        <Form.Control as="select" name="personaid" custom 
+          onChange={ handleChange }>
+          <option value="" key="All Personas">All Personas</option>
+          {personasArray.map(option => (
+            <option value={option.value} key={option.key}>{option.text}</option>
+          ))}
+        </Form.Control>
+      </Form.Group>
+    </Form>
+  );
+}
 
 export default ResourceHub;
