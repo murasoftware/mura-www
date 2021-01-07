8a1a8fcfb74aab5b76af8fbea75e9032ea621971
Scott Widmer <info@scottwidmer.com>
2021-01-07 13:05:54 -0500

    update directory name

--- src/mura/react/UI/ResourceHub/ResourceHub.js
+++ src/mura/react/UI/ResourceHub/ResourceHub.js
@@ -82,7 +82,7 @@ function ResourceHub(props) {
   } else {
       return (
         <div>
-          <h1>Server Side Rendering {thisTitle}</h1>
+          <h1>Static {thisTitle}</h1>
           <RenderFilterForm updateFilter={updateFilter} {...props} curSubtype={curSubtype} curCategoryId={curCategoryId} curPersonaId={curPersonaId} />
           {/* <Collection collection={collection} layout="List" /> */}
           <div className="row collectionLayoutCards row-cols-1 row-cols-sm-2 row-cols-md-2 row-cols-lg-3 row-cols-xl-3">
@@ -161,8 +161,13 @@ const GetCategories = (props) => {
 }
 
 export const getDynamicProps = async props => {
+  console.log('hello');
+  
   const filterProps = await getFilterProps('','','');
   const collection = await getCollection(props,filterProps);
+
+  console.log(collection.getAll());
+
   return{
     collection:collection.getAll(),
     filterprops:filterProps
