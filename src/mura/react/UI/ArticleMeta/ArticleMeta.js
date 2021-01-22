a1ffd8b7ca3c1a567a3f928ffd4f304117634c66
Scott Widmer <info@scottwidmer.com>
2021-01-22 16:44:13 -0500

    cleaning up ArticleMeta and moving credits & tags output to their own components

--- src/mura/react/UI/ArticleMeta/ArticleMeta.js
+++ src/mura/react/UI/ArticleMeta/ArticleMeta.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ItemDate from '@mura/react/UI/Utilities/ItemDate';
-import Badge from 'react-bootstrap/Badge';
-import ItemCategories from '@mura/react/UI/Utilities/ItemCategories';
+import ItemCredits from '@mura/react/UI/Utilities/ItemCredits';
+import ItemTags from '@mura/react/UI/Utilities/ItemTags';
 
 function ArticleMeta(props){
-    const fields = props.fields;
+    console.log('fields ArticleMeta: ' + props.fields);
+    const fields = props.fields ? props.fields : 'Date,Credits,Tags';
     const fieldlist = fields ? fields.toLowerCase().split(",") : [];
     const item = props.content;
-    // const catAssignments = item.getAll().categoryassignments;
-    // console.log(item);
     return (
         <div className="pb-4">
             {
@@ -17,61 +16,24 @@ function ArticleMeta(props){
                     case "date":
                         return (
                             <div className="mura-item-meta__date" key="date">
-                            <ItemDate releasedate={props.content.releasedate} lastupdate={props.content.lastupdate}></ItemDate>
+                            <ItemDate releasedate={item.releasedate} lastupdate={item.lastupdate}></ItemDate>
                             </div>
                         );
                     case "credits":
                         return (
-                            <OutputCredits credits={props.content.credits} key="credits"></OutputCredits>
+                            <ItemCredits credits={item.credits} key="credits" />
                         );
                     case "tags":
                         return (
-                            <OutputTags tags={props.content.tags} key="tags"></OutputTags>
+                            <ItemTags tags={item.tags} key="tags" />
                         );
                     default:
                         return <div className={`mura-item-meta__${field}`} key={field} data-value={props.content[field]}>{props.content[field]}</div>
                     }
                 })
             }
-            {/* {item.categoryid.length &&
-                <ItemCategories categories={catAssignments} />
-            } */}
         </div>
     )
-}
-
-//can be used to format tags specifically or create a component for sitewide usage like ItemDate
-const OutputTags = (props) => {
-    const Tags = props.tags.split(',');;
-    let tagList = [];
-    let tag = '';
-    // console.log(props.tags);
-    for(let i = 0;i < Tags.length;i++) {
-        tag = Tags[i];
-        tagList.push(
-            <Badge variant="primary mr-2">{tag}</Badge>
-        )
-    }
-
-    return tagList;
-}
-//can be used to format credits specifically or create a component for sitewide usage like ItemDate
-const OutputCredits = (props) => {
-    const Credits = props.credits.split(',');
-    let creditsList = [];
-    let credit = '';
-    // console.log(props.tags);
-    for(let i = 0;i < Credits.length;i++) {
-        credit = Credits[i];
-        creditsList.push(
-            <div className="mura-item-meta__credits pb-2">
-                By: <strong>{credit}</strong>
-            </div>
-        )
-    }
-
-    return creditsList;    
-    
 }
 
 export default ArticleMeta;
