2b12e8b37630a8ba07c998069230e86cc7ff71ee
Matt Levine <matthewklevine@gmail.com>
2021-02-15 11:15:42 -0800

    Removing static rendering

--- src/pages/[...page].js
+++ src/pages/[...page].js
@@ -1,28 +1,21 @@
 import React from 'react';
 import Head from 'next/head';
-import { setMuraConfig, MainLayout, MuraJSRefPlaceholder, getMuraProps, getRootPath, getMuraPaths, getSiteName } from '@murasoftware/next-core';
+import { useRouter } from 'next/router';
+import { EditLayout, setMuraConfig, MainLayout, MuraJSRefPlaceholder, getMuraProps, getRootPath, getMuraPaths, getSiteName } from '@murasoftware/next-core';
 import ErrorPage from 'next/error';
 import Body from '../components/Body';
 import muraConfig from 'mura.config';
 
-export async function getStaticPaths() {
-  setMuraConfig(muraConfig);
-  const paths = await getMuraPaths();
 
-  return {
-    paths,
-    fallback: true,
-  };
-}
-
-export async function getStaticProps(context) {
+export async function getServerSideProps(context) {
   setMuraConfig(muraConfig);
-  const props = await getMuraProps(context,false,{expand:'categoryassignments'});
+  const props = await getMuraProps(context,true,{expand:'categoryassignments'});
   return props;
 }
 
 export default function Page(props) {
   setMuraConfig(muraConfig);
+  const router = useRouter();
   /*
    When in a route not defined in static routes it's intitially missing props
   */
@@ -42,54 +35,56 @@ export default function Page(props) {
     return <ErrorPage statusCode="404" />
   } else {
     return (
-      <MainLayout {...props}>  
-        <Head>
-          {/* I wanted to add a "MuraMetaTags" component here but doesn't seem possible inside the <Head> component -- see metaTags branch */}
-          <title>{content.htmltitle} - {getSiteName()}</title>
-          <meta name="description" content={content.metadesc} />
+      <EditLayout {...props}>  
+        <MainLayout {...props} route={`/${router.query.page}`}>  
+          <Head>
+            {/* I wanted to add a "MuraMetaTags" component here but doesn't seem possible inside the <Head> component -- see metaTags branch */}
+            <title>{content.htmltitle} - {getSiteName()}</title>
+            <meta name="description" content={content.metadesc} />
 
-          <meta property="og:site_name" content={getSiteName()} />
-          <meta property="og:title" content={content.htmltitle} />
-          <meta property="og:description" content={content.metadesc} />
-          {content.images && content.images.large &&
-            <meta property="og:image" content={content.images.large} />
-          }
-          <meta property="og:type" content="website" />
-          
-          {content.canonicalurl != '' &&
-            <link rel="canonical" href={content.canonicalurl} />
-          }
+            <meta property="og:site_name" content={getSiteName()} />
+            <meta property="og:title" content={content.htmltitle} />
+            <meta property="og:description" content={content.metadesc} />
+            {content.images && content.images.large &&
+              <meta property="og:image" content={content.images.large} />
+            }
+            <meta property="og:type" content="website" />
+            
+            {content.canonicalurl != '' &&
+              <link rel="canonical" href={content.canonicalurl} />
+            }
 
-          {content.canonicalurl == '' &&
-            <link rel="canonical" href={`${getRootPath()}/${content.filename}`} />
-          }
+            {content.canonicalurl == '' &&
+              <link rel="canonical" href={`${getRootPath()}/${content.filename}`} />
+            }
 
-          <link
-            href={`${getRootPath()}/core/modules/v1/core_assets/css/mura.10.min.css`}
-            rel="stylesheet"
-            key="min"
+            <link
+              href={`${getRootPath()}/core/modules/v1/core_assets/css/mura.10.min.css`}
+              rel="stylesheet"
+              key="min"
+            />
+            <link
+              href={`${getRootPath()}/core/modules/v1/core_assets/css/mura.10.skin.css`}
+              rel="stylesheet"
+              key="skin"
+            />
+
+            <script dangerouslySetInnerHTML={{__html:MuraJSRefPlaceholder}}/>
+          </Head>
+          <div dangerouslySetInnerHTML={{__html:props.codeblocks.header}}/>
+          <div dangerouslySetInnerHTML={{__html:props.codeblocks.bodystart}}/>
+          <Body
+            content={content}
+            moduleStyleData={moduleStyleData}
+            header={header}
+            primarycontent={primarycontent}
+            footer={footer}
+            displayregions={displayregions}
+            props={props}
           />
-          <link
-            href={`${getRootPath()}/core/modules/v1/core_assets/css/mura.10.skin.css`}
-            rel="stylesheet"
-            key="skin"
-          />
-
-          <script dangerouslySetInnerHTML={{__html:MuraJSRefPlaceholder}}/>
-        </Head>
-        <div dangerouslySetInnerHTML={{__html:props.codeblocks.header}}/>
-        <div dangerouslySetInnerHTML={{__html:props.codeblocks.bodystart}}/>
-        <Body
-          content={content}
-          moduleStyleData={moduleStyleData}
-          header={header}
-          primarycontent={primarycontent}
-          footer={footer}
-          displayregions={displayregions}
-          props={props}
-        />
-        <div dangerouslySetInnerHTML={{__html:props.codeblocks.footer}}/>
-      </MainLayout>
+          <div dangerouslySetInnerHTML={{__html:props.codeblocks.footer}}/>
+        </MainLayout>
+      </EditLayout>
     );
   }
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Head from 'next/head';
-import { MainLayout, setMuraConfig, MuraJSRefPlaceholder, getMuraProps, getRootPath, getSiteName } from '@murasoftware/next-core';
+import { EditLayout, MainLayout, setMuraConfig, MuraJSRefPlaceholder, getMuraProps, getRootPath, getSiteName } from '@murasoftware/next-core';
 import Body from '../components/Body';
 import muraConfig from 'mura.config';
 
-export async function getStaticProps(context) {
+export async function getServerSideProps(context) {
   setMuraConfig(muraConfig);
-  const props = await getMuraProps(context,false,{expand:'categoryassignments'});
+  const props = await getMuraProps(context,true,{expand:'categoryassignments'});
   return props;
 }
 
@@ -22,54 +22,56 @@ export default function Page(props) {
   } = props;
   //console.log('content: ', content);
   return (
-    <MainLayout {...props}>
-      <Head>
-        {/* I wanted to add a "MuraMetaTags" component here but doesn't seem possible inside the <Head> component -- see metaTags branch */}
-        <title>{content.htmltitle} - {getSiteName()}</title>
-        <meta name="description" content={content.metadesc} />
+    <EditLayout {...props}>
+      <MainLayout {...props}>
+        <Head>
+          {/* I wanted to add a "MuraMetaTags" component here but doesn't seem possible inside the <Head> component -- see metaTags branch */}
+          <title>{content.htmltitle} - {getSiteName()}</title>
+          <meta name="description" content={content.metadesc} />
 
-        <meta property="og:site_name" content={getSiteName()} />
-        <meta property="og:title" content={content.htmltitle} />
-        <meta property="og:description" content={content.metadesc} />
-        {content.images && content.images.large &&
-          <meta property="og:image" content={content.images.large} />
-        }
-        <meta property="og:type" content="website" />
+          <meta property="og:site_name" content={getSiteName()} />
+          <meta property="og:title" content={content.htmltitle} />
+          <meta property="og:description" content={content.metadesc} />
+          {content.images && content.images.large &&
+            <meta property="og:image" content={content.images.large} />
+          }
+          <meta property="og:type" content="website" />
+          
+          {content.canonicalurl != '' &&
+            <link rel="canonical" href={content.canonicalurl} />
+          }
+
+          {content.canonicalurl == '' &&
+            <link rel="canonical" href={`${getRootPath()}/${content.filename}`} />
+          }
+
+          <link
+            href={`${getRootPath()}/core/modules/v1/core_assets/css/mura.10.min.css`}
+            rel="stylesheet"
+            key="min"
+          />
+          <link
+            href={`${getRootPath()}/core/modules/v1/core_assets/css/mura.10.skin.css`}
+            rel="stylesheet"
+            key="skin"
+          />
+          <script dangerouslySetInnerHTML={{__html:MuraJSRefPlaceholder}}/>
         
-        {content.canonicalurl != '' &&
-          <link rel="canonical" href={content.canonicalurl} />
-        }
-
-        {content.canonicalurl == '' &&
-          <link rel="canonical" href={`${getRootPath()}/${content.filename}`} />
-        }
-
-        <link
-          href={`${getRootPath()}/core/modules/v1/core_assets/css/mura.10.min.css`}
-          rel="stylesheet"
-          key="min"
+        </Head>
+        <div dangerouslySetInnerHTML={{__html:props.codeblocks.header}}/>
+        <div dangerouslySetInnerHTML={{__html:props.codeblocks.bodystart}}/>
+        <Body
+          content={content}
+          moduleStyleData={moduleStyleData}
+          header={header}
+          primarycontent={primarycontent}
+          footer={footer}
+          displayregions={displayregions}
+          props={props}
         />
-        <link
-          href={`${getRootPath()}/core/modules/v1/core_assets/css/mura.10.skin.css`}
-          rel="stylesheet"
-          key="skin"
-        />
-        <script dangerouslySetInnerHTML={{__html:MuraJSRefPlaceholder}}/>
-       
-      </Head>
-      <div dangerouslySetInnerHTML={{__html:props.codeblocks.header}}/>
-      <div dangerouslySetInnerHTML={{__html:props.codeblocks.bodystart}}/>
-      <Body
-        content={content}
-        moduleStyleData={moduleStyleData}
-        header={header}
-        primarycontent={primarycontent}
-        footer={footer}
-        displayregions={displayregions}
-        props={props}
-      />
-      <div dangerouslySetInnerHTML={{__html:props.codeblocks.footer}}/>
-    </MainLayout>
+        <div dangerouslySetInnerHTML={{__html:props.codeblocks.footer}}/>
+      </MainLayout>
+    </EditLayout>
   );
 }
 
