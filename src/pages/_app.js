52a7adc85584306c6c8bcee14b4080f54ab27875
Matt Levine <matthewklevine@gmail.com>
2021-02-15 11:17:13 -0800

    Removing static rendering

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,7 +2,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 import '../scss/custom.scss'
-//import MuraConfig from 'mura.config';
+
 import React from "react";
 
 export default function MuraApp({ Component, pageProps }) {
