e0cb09f2442a16d823bd4f121e0aba7e9f1dc746
Matt Levine <matthewklevine@gmail.com>
2021-10-04 16:55:55 -0700

    Updating Mura modules

--- mura.config.js
+++ mura.config.js
@@ -289,9 +289,11 @@ moduleRegistry.forEach(module => {
 
 Mura.Module.Container.reopen({
 	reset(self, empty) {
-	  self.find('.mura-object:not([data-object="container"])').html('');
-	  self.find('.frontEndToolsModal').remove();
-	  self.find('.mura-object-meta').html('');
+    if(empty){
+      self.find('.mura-object:not([data-object="container"])').html('');
+      self.find('.frontEndToolsModal').remove();
+      self.find('.mura-object-meta').html('');
+    }
 	  var content = self.children('div.mura-object-content');
  
 	  if (content.length) {
