09524b37ebaf9bd29c397bbadc61b44f716c1550
Matt Levine <matthewklevine@gmail.com>
2022-10-20 17:12:15 -0700

    Fixing rewrite logic

--- src/middleware.js
+++ src/middleware.js
@@ -3,17 +3,18 @@ import { NextResponse } from 'next/server'
 export function middleware(request) {
 
     const corePaths=['404','admin','core','plugins','icons','api','images'];
-    request.nextUrl.searchParams.get('mura_upc') === 'false'
-
-    if(!request.nextUrl.pathname.includes('.') && !request.nextUrl.pathname.startsWith("/_") && !corePaths.includes(request.nextUrl.pathname.split('/')[1])){
-       
-        const MURA_OCS = request.nextUrl.searchParams.get('mura_upc') || request.cookies.get('MURA_UPC');
-        const MURA_UPC = request.nextUrl.searchParams.get('mura_ocs') || request.cookies.get('MURA_OCS');
+    let path=request.nextUrl.pathname;
     
-        if(MURA_OCS || MURA_UPC === 'false' || request.nextUrl.searchParams.get('purgeCache')==='true'){
-            return NextResponse.rewrite(new URL(`/_dynamic${request.nextUrl.pathname}`, request.url))
+    if(!path.includes('.') && !path.startsWith("/_") && !corePaths.includes(path.split('/')[1])){
+        const urlSearchParams = new URLSearchParams(request.nextUrl.search);
+        const params = Object.fromEntries(urlSearchParams.entries());
+        const MURA_OCS = params.mura_upc || request.cookies.get('MURA_UPC');
+        const MURA_UPC = params.mura_ocs || request.cookies.get('MURA_OCS');
+    
+        if(MURA_OCS || MURA_UPC === 'false' || params.purgeCache==='true'){
+            return NextResponse.rewrite(new URL(`/_dynamic${path}`, request.url))
         } else {
-            return NextResponse.rewrite(new URL(`/_static${request.nextUrl.pathname}`, request.url))
+            return NextResponse.rewrite(new URL(`/_static${path}`, request.url))
         }
     }
 
