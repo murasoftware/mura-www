9da0554a975f3e3a989786707d2f4e28c133cfb9
Matt Levine <matthewklevine@gmail.com>
2022-12-20 11:34:36 -0800

    Updating Mura modules and fixing cookie reference

--- src/middleware.js
+++ src/middleware.js
@@ -1,47 +1,56 @@
 import { NextResponse } from 'next/server'
 
 export async function middleware(request) {
+
+    const getSiteIdMap=async(request, domain)=>{
+        let siteMap=await fetch(`${process.env.rootpath}/_api/json/v1/default?method=findSiteid&domain=${domain.split(':')[0]}`,{
+            method:'get',
+            headers:{
+                'x-forwarded-host':domain,
+                'cookie':request.headers.get('cookie')
+            }
+        });
+        
+        try{
+            siteMap=await siteMap.json();
+        } catch(e){
+            console.log('Error parsing json', e)
+            siteMap={
+                data:''
+            };
+        }
+    
+        global.siteidmap[domain]={
+            siteid:siteMap.data,
+            time:Date.now()
+        }
+    
+        return  global.siteidmap[domain];
+    }
+    
     try {
         const corePaths=['404','admin','core','plugins','icons','api','images'];
         let path=request.nextUrl.pathname;
-
+      
         if(!path.includes('.') && !path.startsWith("/_") && !corePaths.includes(path.split('/')[1])){
             let tenantPath="";
-
+          
             const params = Object.fromEntries(new URLSearchParams(request.nextUrl.search).entries());
 
             if(process.env.multitenant){
                 global.siteidmap=global.siteidmap || {};
                 const domain=request.headers.get('x-forwarded-host') || request.headers.get('host') || 'localhost';
-                
+              
                 if(domain && (!global.siteidmap[domain] || params.bindtodomain)){
-                    
-                    let siteMap=await fetch(`${process.env.rootpath}/_api/json/v1/default?method=findSiteid`,{
-                        method:'get',
-                        headers:{
-                            'x-forwarded-host':domain,
-                            'cookie':request.headers.get('cookie')
-                        }
-                    });
-                
-                    try{
-                        siteMap=await siteMap.json();
-                    } catch(e){
-                        console.log('Error parsing json', e)
-                        siteMap={data:''};
-                    }
-                    
-                    global.siteidmap[domain]=siteMap.data;
+                    await getSiteIdMap(request, domain);
+                } else if(((Date.now() - global.siteidmap[domain].time) > 1000)){
+                    getSiteIdMap(request, domain);
                 }
                
-                let siteid= (domain && global.siteidmap[domain]) ? global.siteidmap[domain] : '';
+                let siteid= (domain && global.siteidmap[domain].siteid) ? global.siteidmap[domain].siteid : '';
                 
                 if(!siteid){
-                    if(!Array.isArray(ConnectorConfig.siteid)){
-                        siteid=siteid.split(',')[0];
-                    } else {
-                        siteid=ConnectorConfig.siteid;
-                    }
+                    console.log('missing siteid for domain',domain.split(":")[0]);
                 }
 
                 siteid=siteid || 'default';
@@ -51,14 +60,14 @@ export async function middleware(request) {
                 tenantPath=`/${siteid}`;
             }
    
-            const MURA_OCS = params.mura_ocs || request.cookies.get('MURA_OCS');
-            const MURA_UPC = params.mura_upc || request.cookies.get('MURA_UPC');
+            const MURA_OCS = params.mura_ocs || request.cookies.get('MURA_OCS')?.value;
+            const MURA_UPC = params.mura_upc || request.cookies.get('MURA_UPC')?.value;
 
             let adjustedPath=`${tenantPath}${path}`;
             
             //remove trailing slash
             adjustedPath=adjustedPath.replace(/\/+$/, '');
-
+            
             if(MURA_OCS || MURA_UPC === 'false' || params.purgeCache==='true'){
                 return NextResponse.rewrite(new URL(`/_dynamic${adjustedPath}${request.nextUrl.search}`, request.url))
             } else {
